992e9685fa9a09cf9d8d538d9ce1eddfc987b78a
Felicialite19 <felicialite@gmail.com>
2024-11-10 14:31:26 +0400

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -2,15 +2,8 @@ def add_everything_up(a, b):
     sum = 0
     try:
         sum = a + b
-
     except TypeError:
-        if isinstance(a, str) and (isinstance(b, int) or isinstance(b, float)):
-            b = str(b)
-            sum = a+b
-        elif isinstance(b, str) and (isinstance(a, int) or isinstance(a, float)):
-            a = str(a)
-            sum = a + b
-
+        sum = str(a)+str(b)
     return sum
 
 print(add_everything_up(123.456, 'строка'))
